eb3c0b9b51165f7ab01a03f8ec65087b5cb400c1
Dee01code <109283404+Dee01code@users.noreply.github.com>
2022-10-23 09:50:57 +0530

    Time: 65 ms (66.99%), Space: 28.8 MB (36.39%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        
+        int len = nums.size();
+        int real_sum = (len*(len+1))/2;
+        
+        int dup;
+        unordered_map<int,bool> m;
+        for(auto i: nums){
+            if(m.find(i) != m.end()){
+                dup = i;    
+                break;
+            }
+            m[i] = 1;
+        }
+        int actual_sum = 0;
+        for(auto i: nums) actual_sum += i;
+        // cout << dup << actual_sum;
+        return {dup,real_sum-(actual_sum-dup)};
+    }
+};
