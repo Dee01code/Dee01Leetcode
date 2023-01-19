c2fbecd7eed427bc2ad9abe6f2d8baa651e45272
Dee01code <109283404+Dee01code@users.noreply.github.com>
2023-01-20 05:15:59 +0530

    Time: 34 ms (5.01%), Space: 16.2 MB (5.62%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,17 +1,116 @@
 class Solution {
 public:
+    
+class TrieNode{
+public:
+    int data;
+    int terminal;
+    TrieNode* kids[26] = {NULL};
+    TrieNode(char ch){
+        data = ch;
+        terminal = false;
+    }
+};
+
+class Trie{
+public:    
+
+    TrieNode* root;
+    Trie(){ 
+        root = new TrieNode('\0');
+    }
+    /*------------------------------------------------------------*/
+
+    void  insertInner(TrieNode* root, string word){
+        if(word.size() == 0){
+            root -> terminal = true;
+            return;
+        }
+
+        TrieNode* child;
+        int index = word[0]-'a';
+
+        if(root->kids[index] != NULL){
+            child = root->kids[index];
+        }
+        else{
+            child = new TrieNode(word[0]);
+            root -> kids[index] = child;
+        }
+        insertInner(child,word.substr(1));
+    }
+
+    void insert(string word){
+        insertInner(root,word);
+        return;
+    }
+
+    /*------------------------------------------------------------*/
+
+    bool  search(TrieNode* root, string word){
+        if(word.size() == 0){
+            return root -> terminal;
+        }
+
+        TrieNode* child;
+        int index = word[0]-'a';
+
+        if(root->kids[index] != NULL){
+            child = root->kids[index];
+        }
+        else{
+            return false;
+        }
+        return search(child,word.substr(1));
+    }
+    
+    /*------------------------------------------------------------*/
+    int branchNumber(TrieNode* root){
+        int ans = 0;
+        for(int i = 0; i<26; i++){
+            if(root->kids[i] != NULL) ans++;
+        }
+        return ans;
+    }
+    
+    /*------------------------------------------------------------*/
+    
+    void lcp(TrieNode* root, string & ans){
+        if(root->terminal || branchNumber(root) >1) {
+            return;
+        }   
+
+        TrieNode* child;
+        for(int i=0; i<26; i++){
+            if(root->kids[i] != NULL){
+                char ch = 'a'+i;
+                child = root->kids[i]; 
+                ans.push_back(ch);
+            }
+        }
+        
+        lcp(child,ans);
+    }
+    
+    
+    
+    
+};
+
+    
     string longestCommonPrefix(vector<string>& strs) {
         
         int n = strs.size();
         string ans= "";
-        for(int i = 0; i < strs[0].size();  i++){
-            char ch = strs[0][i];
-            for(int j = 1; j<n; j++){
-                char t = strs[j][i];
-                if(ch != t) return ans; 
-            }
-            ans.push_back(strs[0][i]);
+        
+        Trie *t = new Trie();
+        for(auto i : strs){
+            if(i.size()==0) return ans;
+            t->insert(i);
         }
+
+        t->lcp(t->root,ans);
         return ans;
+        
     }
 };
