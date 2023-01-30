d7d247d50113bb0b39d63fbbcb8cb10e0f209abb
Dee01code <109283404+Dee01code@users.noreply.github.com>
2023-01-31 03:13:59 +0530

    Time: 23 ms (32.09%), Space: 18.4 MB (5.51%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,62 +1,32 @@
-// class Solution {
-// public:
-//     vector<vector<int>> insert(vector<vector<int>>& in, vector<int>& newInterval) {
-         
-//         vector<<vector<int>>> ans;
-        
-//         int newst = newInterval[0];
-//         int newend = newInterval[1];
-        
-//         for(int i = 0; i<in.size(); i++){
-            
-//             int st = in[i][0];
-//             int end = in[i][1];
-            
-//             if(newst < st && newend < st){
-                
-//             }
-            
-//             else if(newst < st && newend <= end){
-                
-//             }
-            
-//             else if(newst >= st && newend <= end){
-                
-//             }
-//             else if(newst >= st && newst <= end && newend > end){
-                
-//             }
-            
-//         }
-        
-        
-//     }
-// };
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> v;
-        int n = intervals.size(), i = 0;
-        // push all the intervals till non overlapping interval
-        while(i < n && newInterval[0] > intervals[i][1])
-        {
-            v.push_back(intervals[i]);
+    vector<vector<int>> insert(vector<vector<int>>& in, vector<int>& nin) {
+        
+        vector<vector<int>> temp;
+        int i = 0;
+        while(i<in.size()){
+            if(in[i][0] >= nin[0]) break;
+            temp.push_back(in[i]);
             i++;
         }
-        // merging all the overlapping intervals
-        while(i < n && newInterval[1] >= intervals[i][0])
-        {
-            newInterval[0] = min(newInterval[0], intervals[i][0]);
-            newInterval[1] = max(newInterval[1], intervals[i][1]);
+        
+        temp.push_back(nin);
+        
+        while(i<in.size()){
+            temp.push_back(in[i]);
             i++;
         }
-        v.push_back(newInterval);
-        // psuh all the remaining intervals after merging 
-        while(i < n)
-        {
-            v.push_back(intervals[i]);
-            i++;
+        
+        vector<vector<int>> ans;
+        ans.push_back(temp[0]);
+        for(int i=1; i<temp.size();i++){
+            int lastend = ans.back()[1];
+            int newst = temp[i][0];
+            int newend = temp[i][1];
+            if(lastend >= newst) ans.back()[1] = max(lastend,newend);
+            else ans.push_back(temp[i]);
         }
-        return v;
+        return ans;
+        
     }
 };
