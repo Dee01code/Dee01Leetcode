5a0b3cadb9ee68d033572de9c43d3e1bb91438b3
Dee01code <109283404+Dee01code@users.noreply.github.com>
2023-02-03 00:32:21 +0530

    Time: 27 ms (5.61%), Space: 15.3 MB (5.21%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
+    unordered_map<char, int> m;
+    
+    bool compare(string a, string b){
+        
+        for(int i=0; i<min(a.size(),b.size()); i++){
+            if(m[a[i]] < m[b[i]]) return true;
+            if(m[a[i]] > m[b[i]]) return false;
+        }
+        
+        if(a.size()>b.size()) return false;
+        return true;
+    }
+    
+    
     bool isAlienSorted(vector<string>& words, string order) {
         
-        unordered_map<char, int> m;
         
         for(auto i=0; i<26; i++){
             m[order[i]] = i;
         }
         
-        for(int i=0; i<words.size(); i++){
-            for(int j=i+1; j<words.size(); j++){
-                int s=0;
-                while(s < min(words[i].size(),words[j].size())){
-                    if(m[words[i][s]] > m[words[j][s]]) return false;
-                    if(m[words[i][s]] < m[words[j][s]]) break;
-                    s++;
-                }
-                if(words[i].size() > words[j].size() && words[i].substr(0,s) == words[j]) return false;
-                // cout << words[i].substr(0,s) << " "<< words[j];
-            }
-        }
-        return true;
+        vector<string> checker = words;
+        
+        sort(words.begin(),words.end(),[&](string a, string b){
+            return compare(a,b);
+        });
+        
+        return words == checker;
+        
     }
 };
