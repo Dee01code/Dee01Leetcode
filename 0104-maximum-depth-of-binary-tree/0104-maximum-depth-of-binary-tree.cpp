ad107693978324e0683fd99a6939df7a136ff2d7
Dee01code <109283404+Dee01code@users.noreply.github.com>
2023-02-16 11:13:49 +0530

    Time: 12 ms (48.69%), Space: 19 MB (19.79%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -24,6 +24,35 @@ public:
     }
     
     int maxDepth(TreeNode* root) {
-        return solve(root);
+        // return solve(root);
+        
+        if(!root) return 0;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        int ans = 0;
+        while(!q.empty()){
+            
+            int t = q.size();
+            while(t--){
+                TreeNode* p1 = q.front();
+                q.pop();
+                
+                if(p1->right){
+                    q.push(p1->right);
+                }
+                
+                if(p1->left){
+                    q.push(p1->left);
+                }
+                
+            }
+            ans += 1;
+        }
+        
+        return ans;
+        
+        
     }
 };
