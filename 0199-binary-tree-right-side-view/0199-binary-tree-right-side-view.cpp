2166935c16093317b779218a0ef2a8d67c8aa6de
Dee01code <109283404+Dee01code@users.noreply.github.com>
2023-02-20 01:22:38 +0530

    Time: 11 ms (6.52%), Space: 11.9 MB (91.10%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,42 +1,36 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    vector<int> rightSideView(TreeNode* root) {
-        
-        vector<int> ans;
-        if(!root) return ans;
-        
-        queue<TreeNode*> q;
-        q.push(root);
-        q.push(NULL);
-        
-        
-        while(!q.empty()){
+
+//TOP_VIEW approach
+
+class Solution
+{
+    public:
+    
+    
+        void solve(TreeNode* root, int lvl, vector<int> & ans){
+            if(!root) return;
             
-            TreeNode* temp = q.front();
-            q.pop();
+            if(ans.size() < lvl) ans.push_back(root->val);
+            solve(root->right,lvl+1,ans);
+            solve(root->left,lvl+1,ans);
+        }    
+    
+        vector<int> rightSideView(TreeNode *root)
+        {
+            vector<int> ans;
+            solve(root, 1 , ans);
+            return ans;
             
-            if(!temp) continue;
-            
-            if(temp->left) q.push(temp->left);
-            if(temp->right) q.push(temp->right);
-            
-            if(q.front() == NULL){
-                ans.push_back(temp->val);
-                q.push(NULL);
-            }
             
         }
-        return ans;
-    }
 };
