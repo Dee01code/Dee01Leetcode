bfbf98725b0ff6cc175f5c26a4e44ceb03a2c050
Dee01code <109283404+Dee01code@users.noreply.github.com>
2023-03-26 17:40:24 +0530

    Time: 19 ms (63.72%), Space: 26 MB (89.01%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,16 +11,24 @@
  */
 class Solution {
 public:
-    TreeNode *Tree(vector<int>& in, int x, int y,vector<int>& po,int a,int b){
-        if(x > y || a > b)return nullptr;
-        TreeNode *node = new TreeNode(po[b]);
-        int SI = x;  
-        while(node->val != in[SI])SI++;
-        node->left  = Tree(in,x,SI-1,po,a,a+SI-x-1);
-        node->right = Tree(in,SI+1,y,po,a+SI-x,b-1);
-        return node;
+    
+    TreeNode* solve(vector<int>& in, vector<int>& po, int a, int b, int &c){
+        
+        if(a>b) return NULL;
+        
+        int ind = find(in.begin(), in.end(), po[c]) - in.begin();
+        TreeNode* temp = new TreeNode(po[c--]);
+        
+        temp -> right = solve(in,po, ind+1, b, c);
+        temp -> left = solve(in, po, a, ind-1, c);
+        return temp;
+        
     }
+    
     TreeNode* buildTree(vector<int>& in, vector<int>& po){
-        return Tree(in,0,in.size()-1,po,0,po.size()-1);
+        int a = 0;
+        int b = in.size()-1;
+        int c = b;
+        return solve(in,po,a,b,c);
     }
 };
