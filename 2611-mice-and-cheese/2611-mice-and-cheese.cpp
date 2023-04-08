5fa5b6fc4a3dd0fb1e6dcfa7a886b1aaa95d302f
Dee01code <109283404+Dee01code@users.noreply.github.com>
2023-04-08 23:22:52 +0530

    Time: 1016 ms (5.00%), Space: 173.7 MB (5.98%) - LeetHub

--- 2611-mice-and-cheese/2611-mice-and-cheese.cpp
+++ 2611-mice-and-cheese/2611-mice-and-cheese.cpp
@@ -23,57 +23,6 @@ public:
             else ans += r2[i];
         }
         return ans;
-        // map<int,vector<int>> m;
-        // for(int i = 0; i<r1.size(); i++){
-        //     m[r1[i]].push_back(i);
-        // }
-        // int ans = 0;
-        // vector<int> notUse;
-        // while(k--){
-        //     for(auto i = m.rbegin(); i != m.rend(); i++){
-        //         vector<int> ind = i->second;
-        //         int j = 0;
-        //         while(k>0 && j < ind.size()){
-        //             ans += i->first;
-        //             k--;
-        //             notUse.push_back(ind[j]); 
-        //             j++;
-        //         }
-//         //     }
-//         // }
-//         int n = r1.size();
-//         int totr2 = 0;
-//         int ans = INT_MIN;
-//         for(auto i : r2) totr2 += i;
-//         cout << "TotSum in r2 "<<totr2 << endl;
-//         int winSum1 = 0, winSum2 = 0;
-//         int  i = 0;
-//         while(i<k){
-//             winSum1 += r1[i];
-//             winSum2 += r2[i];
-//             i++;
-//         }
-//         int temp = 0;
-//         temp = totr2 - winSum2 + winSum1;
-//         ans = max(ans,temp);
-//         cout << "Window 1 sum = " << temp<<endl;
-//         cout << i << endl;
-//         for(; i < n; i++){
-//             cout << "Number befor window = " <<r1[i-k];
-//             cout << " Number after window = " <<r1[i];
-//             winSum1 -= r1[i-k];
-//             winSum1 += r1[i];
-            
-//             winSum2 -= r2[i-k];
-//             winSum2 += r2[i];
-            
-//             temp = totr2 - winSum2 + winSum1;
-//             ans = max(ans,temp);
-//             cout << " winSum1 = " << winSum1 << endl;
-//             cout << " winSum2 = " << winSum2 << endl;
-            
-//             cout << "Window sum = " << temp<<endl;
-//         }
-//         return ans;
+        
     }
 };
