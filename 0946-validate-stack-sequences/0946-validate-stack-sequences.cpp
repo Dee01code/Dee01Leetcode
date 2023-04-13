3f984356fb460b2ae0357f9b6f334760ceb2662f
Dee01code <109283404+Dee01code@users.noreply.github.com>
2023-04-13 17:20:43 +0530

    Time: 15 ms (18.21%), Space: 15.4 MB (24.28%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pu, vector<int>& po) {
-        
+
         stack<int> s;
-        
-        int i = 0;
-        int j = 0;
+        int i = 0, j = 0;
         int n = pu.size();
         
-    
-        
         while(i<n && j<n){
-            cout << " Outer loop & Pushing "<< pu[i]<<endl;
             s.push(pu[i++]);
             while(!s.empty() && j<n && s.top() == po[j]){
                 cout << s.top() << " ";
                 s.pop();
-                cout << "stack size = "<<s.size() << endl;
                 j++;
             } 
-            
         }
-        // cout << endl << s.size();
-        // while(!s.empty()){
-        //     cout << s.top() << " ";
-        //     s.pop();
-        // }
+        
         if(s.empty()) return 1;
         return 0;
     }
