ce6adf22bdb3b4814d25442bd1317e52c3173679
Dee01code <109283404+Dee01code@users.noreply.github.com>
2023-04-16 10:36:14 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (64.29%) - LeetHub

---
+++ 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.cpp
@@ -0,0 +1,97 @@
+class Solution {
+public:
+    int addMinimum(string w) {
+        
+        int n = w.size();
+        if(n==1) return 2;
+        
+        int cnt = 0;
+        for(int i = 0; i<n;){
+            if(w[i] == 'a'){
+                if(i < n-1){ 
+                    if(w[i+1] == 'c'){
+                        cnt++;
+                        i+=2;
+                    }
+                    else if(w[i+1] == 'a') {cnt += 2; i++;}
+                    else i++;
+                }
+                else{
+                    cnt+=2;
+                    i++;
+                }
+            }
+            else if(w[i] == 'b'){
+                if(i > 0 && i<n-1){
+                    if(w[i-1] != 'a' && w[i+1] == 'c'){
+                        cnt++;
+                        i+=2;
+                    }
+                    else if(w[i-1] == 'a' && w[i+1] == 'c'){
+                        i+=2;
+                    }
+                    else if(w[i-1] == 'a' && w[i+1] != 'c'){
+                        cnt++;
+                        i++;
+                    }
+                    else if(w[i-1] != 'a' && w[i+1] != 'c'){
+                        cnt += 2;
+                        i++;
+                    }
+                    else i++;
+                }
+                else if(i==0 && i<n-1){
+                    if(w[i+1] == 'c'){
+                        cnt++; 
+                        i += 2;
+                    }
+                    else if(w[i+1] == 'b'){
+                        cnt+=2;
+                        i++;
+                    }
+                    else if(w[i+1] == 'a'){
+                        i++;
+                        cnt+=2; 
+                    }
+                    else i++;
+                }
+                else if(i > 0 && i == n-1){
+                    if(w[i-1] == 'a'){
+                        cnt++;
+                    }
+                    else if(w[i-1] == 'b'){
+                        cnt += 2;
+                    }
+                    else if(w[i-1] == 'c'){
+                        cnt += 2;
+                    }
+                    i++;
+                }
+                else i++;
+            }
+            else{
+                if(i == 0){
+                    cnt += 2;
+                    i++;
+                }
+                else if(i > 0 && i<n-1){
+                    if(w[i-1] == 'c'){
+                        cnt += 2;
+                        i++;
+                    }
+                    else i++;
+                }
+                else if(i == n-1){
+                    if(w[i-1] == 'c'){
+                        cnt += 2;
+                        i++;
+                    }
+                    else i++;
+                }
+                else i++;
+            }
+        }
+        return cnt;
+        
+    }
+};
