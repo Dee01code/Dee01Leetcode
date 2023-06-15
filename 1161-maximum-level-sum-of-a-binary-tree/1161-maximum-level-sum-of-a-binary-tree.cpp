cc337f4cffc0652ce1aa0b41fd6715f6630398b9
Dee01code <109283404+Dee01code@users.noreply.github.com>
2023-06-15 22:08:59 +0530

    Time: 203 ms (65.21%), Space: 107.3 MB (59.25%) - LeetHub

---
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        vector<int> sum;
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
+        int tot = 0;
+        while(!q.empty()){
+            TreeNode* temp = q.front();
+           
+            q.pop();
+            
+            if(!temp){
+                if(!q.empty()) q.push(NULL);
+                cout << tot << endl;
+                sum.push_back(tot);
+                tot = 0;
+            }
+            
+            else{ 
+                tot += temp->val;
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);                
+            }
+            
+        }
+        
+        int maxi = INT_MIN;
+        int ind = -1;
+        
+        for(int i = 0; i<sum.size();i++){
+            cout << sum[i] << " ";
+            if(sum[i] > maxi){
+                maxi = sum[i];
+                ind = i+1;
+            }
+        }
+        
+        return ind;
+    }
+};
