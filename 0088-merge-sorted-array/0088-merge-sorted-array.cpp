b3171d8ca4d93ed6ffbe0cf0143170bc6e979457
Dee01code <109283404+Dee01code@users.noreply.github.com>
2023-08-15 23:54:54 +0530

    Time: 3 ms (52.50%), Space: 9.1 MB (14.08%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,23 +2,38 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
-        int i = m-1;
-        int k = n+m-1;
-        while(i>=0){
-            nums1[k--] = nums1[i--];
+//         int i = m-1;
+//         int k = n+m-1;
+//         while(i>=0){
+//             nums1[k--] = nums1[i--];
+//         }
+        
+//         int j=0;
+//         i = ++k;
+//         k = 0;
+//         while(i < n+m && j<n){
+//             if(nums1[i] < nums2[j]) nums1[k++] = nums1[i++];
+//             else nums1[k++] = nums2[j++];
+//         }
+        
+//         while(i < n+m) nums1[k++] = nums1[i++];
+//         while(j < n) nums1[k++] = nums2[j++];
+        
+//         return;
+        
+        
+        int k = m;
+        int j = 0;
+        while(k < m+n && j<n){
+            nums1[k++] = nums2[j++];
         }
         
-        int j=0;
-        i = ++k;
-        k = 0;
-        while(i < n+m && j<n){
-            if(nums1[i] < nums2[j]) nums1[k++] = nums1[i++];
-            else nums1[k++] = nums2[j++];
-        }
+        for(int i = 0; i<m+n; i++) cout << nums1[i] << " ";
+        cout << endl;
         
-        while(i < n+m) nums1[k++] = nums1[i++];
-        while(j < n) nums1[k++] = nums2[j++];
-        
+        sort(nums1.begin(),nums1.end());
         return;
+        
+        
     }
 };
