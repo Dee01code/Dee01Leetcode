36a31f6258ca41960a737a4b4a31dfd5105a53ef
Dee01code <109283404+Dee01code@users.noreply.github.com>
2023-09-09 15:50:47 +0530

    Time: 4 ms (28.06%), Space: 6.6 MB (85.45%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -13,8 +13,23 @@ public:
         
     }
     
+    int solveTab(vector<int>& nums, int t){
+        int dp[t+1];
+        memset(dp , 0, sizeof(dp));
+     
+        for(int i = 1; i<=t; i++){
+            for(int j = 0; j<nums.size(); j++){
+                if(t-nums[j] >= 0)
+                    dp[i] += dp[t-nums[j]];
+            }
+        }
+        return dp[t];
+        
+    }
+    
     int combinationSum4(vector<int>& nums, int target) {
         memset(dp,-1,sizeof(dp));
         return solve(nums, target);
+        return solveTab(nums, target);
     }
 };
