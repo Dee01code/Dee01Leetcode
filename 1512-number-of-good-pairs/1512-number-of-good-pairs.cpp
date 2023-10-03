94761cb100b736f9b76d93061979486de27d99f9
Dee01code <109283404+Dee01code@users.noreply.github.com>
2023-10-04 00:42:17 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (7.10%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int n = nums.size();
+        int cnt = 0;
+        for(int i = 0; i<n; i++)
+            for(int j = i+1; j<n; j++)
+                cnt += (nums[i] == nums[j]) ? 1 : 0;
+        return cnt;
+    }
+};
