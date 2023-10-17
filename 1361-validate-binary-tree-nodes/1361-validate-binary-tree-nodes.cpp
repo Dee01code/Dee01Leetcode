87e5c250eb20d58ce1a99e2b91ed425ca5ede67f
Dee01code <109283404+Dee01code@users.noreply.github.com>
2023-10-17 22:43:16 +0530

    Time: 223 ms (5.01%), Space: 46.4 MB (12.92%) - LeetHub

---
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    bool validateBinaryTreeNodes(int n, vector<int>& left, vector<int>& right) {
+        
+        // 1. node can have only single parent
+        // 2. only one root i.e. More than 1 component will not be acceptable
+        // 3. node visited count at last should be equal to n.
+        
+        unordered_map<int,int> CtoP;
+        unordered_map<int,vector<int>> adj;
+        
+        for(int i = 0; i<n; i++){
+            int lc = left[i];
+            int rc = right[i];
+            
+            if(lc != -1){
+                
+                //using 1st cond
+                cout << "Lc" << endl;
+                if(CtoP.count(lc)) return false;
+                
+                adj[i].push_back(lc);
+                CtoP[lc] = i;
+            }
+            if(rc != -1){
+            
+                //using 1st cond
+                cout << "Lc" << endl;
+                if(CtoP.count(rc)) return false;
+                
+                adj[i].push_back(rc);
+                CtoP[rc] = i;
+            }
+        }
+        
+        int root = -1;
+        for(int i = 0; i<n; i++){
+            if(CtoP.count(i) == 0){
+                
+                //using 3rd condition
+                cout << "Root" << endl;
+                if(root != -1) return false;
+                
+                root = i;
+            }
+        }
+        
+        if(root == -1) return false;
+        
+        queue<int> q;
+        vector<int> marked(n,0);
+        q.push(root);
+        int cnt = 0;
+        
+        while(!q.empty()){
+            
+            int len = q.size();
+            int node = q.front();
+            q.pop();
+            
+            while(len--){
+                if(!marked[node]){
+                    for(auto i : adj[node]) q.push(i);
+                    marked[node] = 1;
+                    cnt++;
+                }
+            }
+        }
+        
+        cout << "End" << endl;
+        if(cnt != n) return false;
+        return true;
+        
+    }
+};
