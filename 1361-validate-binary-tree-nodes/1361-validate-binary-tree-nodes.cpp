3a844d410879c54db6a9005d7a51cf02910f3c0a
Dee01code <109283404+Dee01code@users.noreply.github.com>
2023-10-17 23:34:41 +0530

    Time: 32 ms (83.13%), Space: 32.3 MB (96.20%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
@@ -1,75 +1,115 @@
 class Solution {
 public:
+    
+    int component;
+    vector<int> parent;
+    
+    int findParent(int node){
+        if(parent[node] == node) return node;
+        return parent[node] = findParent(parent[node]);
+    }
+    
+    bool join(int par, int child){
+        
+        if(findParent(child) != child) return false;
+        
+        if(findParent(par) == child) return false;
+            
+        parent[child] = par;
+        component -= 1;
+        
+        return true;
+    }
+    
     bool validateBinaryTreeNodes(int n, vector<int>& left, vector<int>& right) {
         
-        // 1. node can have only single parent
-        // 2. only one root i.e. More than 1 component will not be acceptable
-        // 3. node visited count at last should be equal to n.
+        // Using DSU
+            
+        parent.resize(n);
+        for(int i = 0; i<n; i++) parent[i] = i;
         
-        unordered_map<int,int> CtoP;
-        unordered_map<int,vector<int>> adj;
+        component = n;
         
         for(int i = 0; i<n; i++){
+            
             int lc = left[i];
             int rc = right[i];
             
-            if(lc != -1){
-                
-                //using 1st cond
-                cout << "Lc" << endl;
-                if(CtoP.count(lc)) return false;
-                
-                adj[i].push_back(lc);
-                CtoP[lc] = i;
-            }
-            if(rc != -1){
-            
-                //using 1st cond
-                cout << "Lc" << endl;
-                if(CtoP.count(rc)) return false;
-                
-                adj[i].push_back(rc);
-                CtoP[rc] = i;
-            }
+            if(lc != -1 && join(i,lc) == 0) return 0;
+            if(rc != -1 && join(i,rc) == 0) return 0;  
         }
         
-        int root = -1;
-        for(int i = 0; i<n; i++){
-            if(CtoP.count(i) == 0){
-                
-                //using 3rd condition
-                cout << "Root" << endl;
-                if(root != -1) return false;
-                
-                root = i;
-            }
-        }
+        if(component != 1) return 0;
+        return 1;
         
-        if(root == -1) return false;
+//         // 1. node can have only single parent
+//         // 2. only one root i.e. More than 1 component will not be acceptable
+//         // 3. node visited count at last should be equal to n.
         
-        queue<int> q;
-        vector<int> marked(n,0);
-        q.push(root);
-        int cnt = 0;
+//         unordered_map<int,int> CtoP;
+//         unordered_map<int,vector<int>> adj;
         
-        while(!q.empty()){
+//         for(int i = 0; i<n; i++){
+//             int lc = left[i];
+//             int rc = right[i];
             
-            int len = q.size();
-            int node = q.front();
-            q.pop();
+//             if(lc != -1){
+                
+//                 //using 1st cond
+//                 cout << "Lc" << endl;
+//                 if(CtoP.count(lc)) return false;
+                
+//                 adj[i].push_back(lc);
+//                 CtoP[lc] = i;
+//             }
+//             if(rc != -1){
             
-            while(len--){
-                if(!marked[node]){
-                    for(auto i : adj[node]) q.push(i);
-                    marked[node] = 1;
-                    cnt++;
-                }
-            }
-        }
+//                 //using 1st cond
+//                 cout << "Lc" << endl;
+//                 if(CtoP.count(rc)) return false;
+                
+//                 adj[i].push_back(rc);
+//                 CtoP[rc] = i;
+//             }
+//         }
         
-        cout << "End" << endl;
-        if(cnt != n) return false;
-        return true;
+//         int root = -1;
+//         for(int i = 0; i<n; i++){
+//             if(CtoP.count(i) == 0){
+                
+//                 //using 3rd condition
+//                 cout << "Root" << endl;
+//                 if(root != -1) return false;
+                
+//                 root = i;
+//             }
+//         }
+        
+//         if(root == -1) return false;
+        
+//         queue<int> q;
+//         vector<int> marked(n,0);
+//         q.push(root);
+//         int cnt = 0;
+        
+//         while(!q.empty()){
+            
+//             int len = q.size();
+//             int node = q.front();
+//             q.pop();
+            
+//             while(len--){
+//                 if(!marked[node]){
+//                     for(auto i : adj[node]) q.push(i);
+//                     marked[node] = 1;
+//                     cnt++;
+//                 }
+//             }
+//         }
+        
+//         cout << "End" << endl;
+//         if(cnt != n) return false;
+//         return true;
         
     }
 };
