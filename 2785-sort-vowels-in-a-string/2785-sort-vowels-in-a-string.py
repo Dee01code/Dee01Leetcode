9d586a8d4eb865066c8ba01215dad2accf6782b7
Dee01code <109283404+Dee01code@users.noreply.github.com>
2023-11-13 17:45:33 +0530

    Time: 2918 ms (5.10%), Space: 19.2 MB (51.18%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
@@ -4,13 +4,13 @@ class Solution:
         vowels = []
         
         for i in inp:
-            if i.lower() in ['a','e','i','o','u']:
+            if i.lower() in {'a','e','i','o','u'}:
                 vowels.append(i) 
         
         vowels.sort()
         
         for i in range(len(inp)):
-            if inp[i].lower() in ['a','e','i','o','u']:
+            if inp[i].lower() in {'a','e','i','o','u'}:
                 inp[i] = vowels[0]
                 vowels.pop(0) 
                 
