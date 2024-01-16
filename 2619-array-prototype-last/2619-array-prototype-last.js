85600f6ad1014855d016389894c3335a6d75296e
Dee01code <109283404+Dee01code@users.noreply.github.com>
2024-01-17 02:40:27 +0530

    Time: 43 ms (90.67%), Space: 42.3 MB (6.51%) - LeetHub

---
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -0,0 +1,15 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    let len = this.length;
+    if(len > 0){
+        return this[len-1];
+    }
+    else return -1;
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
